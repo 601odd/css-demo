fda722df2cd5b20a59d653849149d131079335f4
ouxingyuan <oddpro601@gmail.com>
2023-08-30 22:29:02 +0800

    feat: ✨ add pie

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Shape from './demo/shape/Shape.jsx'
 import Navbar from './demo/navbar/Navbar.jsx'
+import Pie from './demo/pie/Pie.jsx'
 import './App.css'
 
 function App() {
 	return (
 		<>
-			<Navbar />
+			<Pie />
 			{/* <Shape></Shape> */}
 		</>
 	)
---
+++ src/demo/pie/Pie.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './pie.less'
+export default function Pie() {
+  return <>
+  <div id='pie'></div>
+  <div className="pie-pro"></div>
+  </>
+}
