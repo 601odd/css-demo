427d90e649fe2dda7594ddc6d0de632303f79472
ouxingyuan <oddpro601@gmail.com>
2023-09-02 21:59:15 +0800

    feat:  ✨add position demo

---
+++ src/demo/position/Position.jsx
@@ -0,0 +1,9 @@
+import './position.less'
+export default function Position() {
+	return (
+		<div className="contatiner">
+			<div className="position"></div>
+			{/* <div style={{ height: 400 }}></div> */}
+		</div>
+	)
+}
